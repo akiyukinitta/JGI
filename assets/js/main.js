5925ac2ba9c079ae0ccd7b8cdc4827297a17f43d
akiyukinitta <nitta@j-cool.fm>
2022-03-30 19:53:25 +0900

    topic追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,29 @@
-$(function(){
+/*
+var $filters = $('.js-tab-filter [data-filter]'),
+    $boxes = $('.js-tab-boxes [data-category]');
 
-  //任意のタブにURLからリンクするための設定
-  function GethashID (hashIDName){
-    if(hashIDName){
-      //タブ設定
-      $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-        var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
-        if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-          var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-          $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-          $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-          //表示させるエリア設定
-          $(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-          $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
-        }
+$filters.on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  $filters.removeClass('tab-active');
+  $this.addClass('tab-active');
+
+  var $filterColor = $this.attr('data-filter');
+
+  if ($filterColor == 'all') {
+    $boxes.removeClass('is-animated')
+      .fadeOut().promise().done(function() {
+        $boxes.addClass('is-animated').fadeIn();
       });
-    }
+  } else {
+    $boxes.removeClass('is-animated')
+      .fadeOut().promise().done(function() {
+        $boxes.filter('[data-category = "' + $filterColor + '"]')
+          .addClass('is-animated').fadeIn();
+      });
   }
-  
-  //タブをクリックしたら
-  $('.tab a').on('click', function() {
-    var idName = $(this).attr('href'); //タブ内のリンク名を取得	
-    GethashID (idName);//設定したタブの読み込みと
-    return false;//aタグを無効にする
-  });
-  
-  
-  // 上記の動きをページが読み込まれたらすぐに動かす
-  $(window).on('load', function () {
-      $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-      $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
-    var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-    GethashID (hashName);//設定したタブの読み込み
-  });
-  });
-     
-
+});
+*/
 
 //tabend
 
@@ -79,6 +67,7 @@ $('#page-top a').click(function () {
 });
 
 // スムーススクロール
+/*
 $(function () {
   $('.anchor').click(function () {
     var time = 500;
@@ -89,6 +78,7 @@ $(function () {
     return false;
   });
 });
+*/
 $('.slick').slick({
   dots: true,
   infinite: true,
